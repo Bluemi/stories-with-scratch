6522b1d1f94c82a4071da1b56271b97f0cc1bc46
Bluemi <bruno.schilling@protonmail.ch>
2026-04-16 10:28:53 +0200

    remove unused template

--- src/start-scene.ts
+++ src/start-scene.ts
@@ -1,10 +1,7 @@
 import {Scene, Context, Sprite, Label} from 'dkwdpil';
 
 export class StartScene extends Scene {
-    startSprite: Sprite = new Sprite("playSprite", "play.png", 0, 0, {size: 12.0});
-    editSprite: Sprite = new Sprite("editSprite", "edit.png", -30, 16, {size: 3.0});
-    eventLabel: Label = new Label("eventsLabel", "Events", -30, 13, {fontsize: 1.0});
-    testLabel1: Label = new Label("testLabel", "Example Title", 0, 15, {fontsize: 3.0, horizAlign: "center", vertAlign: "center"});
+    title: Label = new Label("testLabel", "Scratch", 0, 15, {fontsize: 2.5, horizAlign: "center", vertAlign: "center"});
 
     init(context: Context): void {
         context.imageMode(context.CENTER);
@@ -15,20 +12,11 @@ export class StartScene extends Scene {
     update(c: Context) {
         c.background(235);
 
-        if (this.startSprite.clicked) {
-            c.nextScene = "nextScene";
-        }
-        if (this.editSprite.clicked) {
+        if (c.keyJustPressed("KeyE")) {
             c.nextScene = "editScene";
         }
-
-        if (this.eventLabel.hovered) {
-            this.eventLabel.color = [255, 0, 0];
-            if (this.eventLabel.clicked) {
-                c.nextScene = "eventScene";
-            }
-        } else {
-            this.eventLabel.color = [0, 0, 0];
+        if (c.keyJustPressed("KeyV")) {
+            c.nextScene = "eventScene";
         }
     }
 }
